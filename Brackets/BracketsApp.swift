3e6be5efc953a5e4d291a97e927f742479edf92c
Caleb Hearth <caleb@calebhearth.com>
2025-02-04 09:04:49 -0700

    Initial Commit

---
+++ Brackets/BracketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BracketsApp.swift
+//  Brackets
+//
+//  Created by Caleb Hearth on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct BracketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BracketsTests/BracketsTests.swift
@@ -0,0 +1,16 @@
+//
+//  BracketsTests.swift
+//  BracketsTests
+//
+//  Created by Caleb Hearth on 2/4/25.
+//
+
+import Testing
+
+struct BracketsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
